0a3e44e9978933def9156aa092457a0e50c0d119
saitejamandadi99 <saitejamandadi99>
2025-05-19 12:27:33 +0530

    added the basic backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 5000
+
+
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+
+.then(()=>{
+    console.log('MongoDB connected ')
+})
+.catch(e=>{
+    console.error('Error Occured while connecting to MongoDB: ', e)
+})
+
+app.get('/', (req,res)=>{
+    res.send('Application is running in the backend. ')
+})
+
+app.listen(port, ()=>{
+    console.log(`Application is running on http://localhost:${port}`)
+})
+
+
+
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Attach user info to req
+      req.user = await User.findById(decoded.id).select('-password');
+      console.log('authMiddleware → req.user:', req.user);
+
+
+      if (!req.user) {
+        return res.status(401).json({ message: 'Not authorized, user not found' });
+      }
+
+      return next(); // we stop here if all is well
+    } catch (error) {
+      console.error('Error in authMiddleware:', error);
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  // If token is missing or invalid format
+  return res.status(401).json({ message: 'Not authorized, no token provided' });
+};
+
+module.exports = authMiddleware;
